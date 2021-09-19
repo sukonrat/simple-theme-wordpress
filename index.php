3127d962c81dc2f06286ec2a6b7277ceac1ab069
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-09-19 20:44:01 +1000

    initial head theme

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+   
+</head>
+<body>
+    
+</body>
+</html>
